dc79dc4bac2c0f918e5c429d8cf44203f689ea39
Shaimaa0Badawy <shaimaabadawy488@gmail.com>
2025-05-17 19:39:31 +0300

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,1 +1,28 @@
 package pages;
+
+import org.opeqa.slenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage{
+  WebDriver driver;
+    
+public LoginPage(WebDriver driver){
+ this.driver = driver;
+ }
+
+//locators
+By usernameField = By.Id("");
+By passwordField = By.Id("");
+ByloginButton = By.Id("");
+
+//methods
+public void enterUsername(String Username){ drver.findElementg(usernameField).sendKeys("");
+  }
+public void enterpassword(String password){driver.findElement(passwordField).sendKeys("");
+   }
+public void clicklogin(){
+driver.findElement(loginButton).click();    }
+}
+
+
